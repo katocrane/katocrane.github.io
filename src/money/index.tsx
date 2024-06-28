c4c08693634340773f58d9f17a89ae9836a6703d
katocrane <2905638689@qq.com>
2024-06-28 16:39:43 +0800

    fix: 还原实现

--- src/money/index.tsx
+++
@@ -1,5 +0,0 @@
-const Money = () => {
-    return <h2>Money Page</h2>;
-  };
-  
-  export default Money;
--- src/play/index.tsx
+++
@@ -1,5 +0,0 @@
-const Play = () => {
-  return <h2>Play Page</h2>;
-};
-
-export default Play;
